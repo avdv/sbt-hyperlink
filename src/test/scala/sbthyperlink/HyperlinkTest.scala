4d21bc64dab57bfe150087319265db9c3044803c
Claudio Bley <claudio.bley@gmail.com>
2018-07-16 22:44:31 +0200

    Introduce `HyperlinkAction` trait
    This is the basic interface for defining hyperlinks by a single abstract method.
    
    Add `FileAction` and `TermlinkAction` implentations and adjust README.md.

--- src/test/scala/sbthyperlink/HyperlinkTest.scala
+++ src/test/scala/sbthyperlink/HyperlinkTest.scala
@@ -6,15 +6,25 @@ import utest._
 
 object HyperlinkTest extends TestSuite {
   val tests = Tests {
-    'testRegexMatch - {
+    'testFileAction - {
       val r = "(/abc)/(test)/(hello.scala):(\\d+:\\d+)".r("basedir", "path", "file", "pos")
       val m = r.findFirstMatchIn("/abc/test/hello.scala:55:3")
 
       assert(m.isDefined)
 
-      val (link, text) = HyperlinkPlugin.fileLink(m.get).get
+      val (link, text) = FileAction(m.get).get
 
       assert(link == "file:///abc/test/hello.scala", text == "test/hello.scala:55:3")
+    }
+    'testTermlinkAction - {
+      val r = "(/abc)/(test)/(hello.scala):(\\d+:\\d+)".r("basedir", "path", "file", "pos")
+      val m = r.findFirstMatchIn("/abc/test/hello.scala:55:3")
+
+      assert(m.isDefined)
+
+      val (link, text) = TermlinkAction(m.get).get
+
+      assert(link == "termlink:///abc/test/hello.scala:55:3", text == "test/hello.scala:55:3")
     }
   }
 }
